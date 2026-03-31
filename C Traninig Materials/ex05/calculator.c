ab433f21744eb375b8ee3bc68de104bae0d6380f
YOUNES ADNANE <younessadnane1m@gmail.com>
2026-03-31 11:01:45 +0100

    project: full calculator app using if-else logic

---
+++ C Traninig Materials/ex05/calculator.c
@@ -0,0 +1,60 @@
+/* 
+=== Calculator App With C Lang ===
+   num1 + num2 = rseult
+
+   What we nedd from the user ?
+   #num1
+   #num2
+   #operation ("+" "-" "*" "/" "%" )
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int num1;
+    int num2;
+    char op;
+
+    printf("Enter num1 :");
+    scanf("%d", &num1);
+
+    printf("Enter Operation :");
+    scanf("%s", &op);
+
+    printf("Enter num2 :");
+    scanf("%d", &num2);
+
+    if(op == '+')
+    {
+        printf("Result = %d", num1 + num2);
+    }
+
+    else if(op == '-')
+    {
+        printf("Result = %d", num1 - num2);
+    }
+
+    else if(op == '*')
+    {
+        printf("Result = %d", num1 * num2);
+    }
+
+    else if(op == '/')
+    {
+        printf("Result = %d", num1 / num2);
+    }
+
+    else if(op == '%')
+    {
+        printf("Result = %d", num1 % num2);
+    }
+
+    else
+    {
+        printf("Check you'r operation!");
+    }
+
+    return 0;
+
+}
